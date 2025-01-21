2244c256db8ae64c597d1d0edcaac5b6aa1643ea
Vittal Katwe <109062125+vittalkatwe@users.noreply.github.com>
2025-01-21 23:30:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import os
-os.system("vllm serve 'vikhyatk/moondream2' --host 0.0.0.0 --port 8000 --device cpu")
+os.system("vllm serve 'vikhyatk/moondream2' --host 0.0.0.0 --port 8000 --device cpu --trust-remote-code")
+
+
